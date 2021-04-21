028d16a43906f36524cacb9ae88fc70efdf671d8
scroodge <scroodgemac@gmail.com>
2021-04-21 17:02:39 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,23 @@
 class minecraft {
-  file {'opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
   }
-  file {' /opt/minecraft/server.jar':
+  file {‘/opt/minecraft/server.jar’:
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    source      => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
   }
-  package {'java':
+  package {‘java’:
     ensure => present,
   }
-  file {' /opt/minecraft/eula.txt':
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true',
+    content => ‘eula=true’
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-  }  
-  service {'minecraft':
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
   }
